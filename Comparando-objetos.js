6b4412de4b1c58f460c63e9979b134316b7ee3e8
Heitor Carvalho de Almeida Neto <heitorh3@gmail.com>
2017-03-14 19:16:14 -0300

    Comparando objetos

---
+++ Comparando-objetos.js
@@ -0,0 +1,50 @@
+var joao = {
+	nome: 'joao',
+	idade: 25,
+	email:'teste@gmail.com',
+	endereco:{
+		logradouro:'Av Brasil',
+		numero: 100,
+		complemento: 'Apt 202',
+		pais:{
+			nome: 'Brasil',
+			cidade: 'Rio de Janeiro',
+			estado: 'RJ'
+		}
+	},
+	igualA:function(obj){
+		return this.email === obj.email;
+	},
+	cidadesIguais: function(obj){
+		return this.endereco.pais.cidade === obj.endereco.pais.cidade;
+	}
+
+}
+
+var maria = {
+	nome: 'Maria',
+	idade: 22,
+	email:'teste3@gmail.com',
+	endereco:{
+		logradouro:'Av Pedro 2',
+		numero: 598,
+		complemento: 'casa',
+		pais:{
+			nome: 'Brasil',
+			cidade: 'Belo Horizonte',
+			estado: 'MG'
+		}
+	},
+	igualA:function(obj){
+		return this.email === obj.email;
+	},
+	cidadesIguais: function(obj){
+		return this.endereco.pais.cidade === obj.endereco.pais.cidade;
+	}		
+}
+
+console.log('João e Maria tem o mesmo email?', joao.igualA(maria) ? 'Sim' : 'Não');
+
+console.log('-------------------------------------------------------');
+
+console.log('João e Maria são da mesma?', joao.cidadesIguais(maria) ? 'Sim' : 'Não');
